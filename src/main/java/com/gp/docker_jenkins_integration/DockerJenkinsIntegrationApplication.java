d2a0dfed4639eef17288c8f4c4f2a435c34040ef
ACER <htgopalsukhmai@gmail.com>
2026-05-06 22:58:06 +0530

    first commit

---
+++ src/main/java/com/gp/docker_jenkins_integration/DockerJenkinsIntegrationApplication.java
@@ -0,0 +1,20 @@
+package com.gp.docker_jenkins_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+	}
+
+	@GetMapping("/docker-jenkins-test")
+	public String dockerIntegrationTest(){
+		return "Docker jenkins integration working now";
+	}
+}
---
+++ src/test/java/com/gp/docker_jenkins_integration/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gp.docker_jenkins_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// TODO document why this method is empty
+	}
+
+}
